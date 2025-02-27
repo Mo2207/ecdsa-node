769ad7b9454f69585c880b58c35b6f31fee78b7b
Gabe Morris <gabemorris1042@gmail.com>
2025-02-27 10:42:59 -0400

    added signing function on clientside to send signature to server

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -4,7 +4,6 @@ import { hexToBytes, toHex, utf8ToBytes } from "ethereum-cryptography/utils.js";
 import { keccak256 } from "ethereum-cryptography/keccak.js";
 
 
-
 const privateKey = secp256k1.utils.randomPrivateKey();
 const publicKey = secp256k1.getPublicKey(privateKey); // publicKey is very long so change it to a keccak256 address
 
